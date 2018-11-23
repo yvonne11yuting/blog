df0970a0979bd81d9fd808af278f4b770e72485d
yvonne11yuting <yvonne11yuting@gmail.com>
2018-11-23 13:35:21 +0800

    index add markdown list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
+import PostLink from "../components/post_link"
+import Layout from "../components/layout"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Is Title</h1>
-    <p>部落格123</p>
-    <div>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  return (
+    <Layout>
+      <div>{Posts}</div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
