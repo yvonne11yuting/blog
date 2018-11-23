ead304b1e8f92a80987953dc54345b1036baf366
yvonne11yuting <yvonne11yuting@gmail.com>
2018-11-23 11:20:33 +0800

    blog init

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: `Yvonne`,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Yvonne',
+        short_name: 'Yvonne',
+        start_url: '/',
+        background_color: '#00a5be',
+        theme_color: '#00a5be',
+        display: 'minimal-ui',
+        icon: 'src/images/y_icon.png', // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+  pathPrefix: `/blog`,
+}
