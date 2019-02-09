c9c1b9a4db66e69fde246f45c1e5294f22aa124a
yvonne11yuting <yvonne11yuting@gmail.com>
2019-02-09 19:10:35 +0800

    modify: page styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,22 @@ import { Link } from 'gatsby'
 
 const Header = ({siteTitle}) => (
   <header className="header">
-        <h1>
-          <Link to="/">
-            {siteTitle}
-          </Link>
-        </h1>
-        <nav>
-          <Link to="/posts">文章列表</Link>
-          <Link to="/tags">標籤索引</Link>
-        </nav>
-      </header>
+    <div class="header-container">
+      <Link
+        class="header-title"
+        activeClassName="header-title"
+        to="/">{siteTitle}</Link>
+      <nav>
+        <Link
+          class="header-link"
+          to="/posts">文章列表</Link>
+        <Link
+          class="header-link"
+          to="/tags"
+        >標籤索引</Link>
+      </nav>
+    </div>
+  </header>
 )
 
 export default Header
