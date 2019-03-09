0c1005bd60ad9d1c679c0e17c8c477669498b732
yvonne11yuting <yvonne11yuting@gmail.com>
2019-03-09 17:52:02 +0800

    refactor: move posts directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/pages/posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
