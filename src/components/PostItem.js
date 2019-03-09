9137f3e9e4d10d6b489e05a7f68dc570d13801db
yvonne11yuting <yvonne11yuting@gmail.com>
2019-03-09 17:08:56 +0800

    refactor: modfiy title tag

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -3,13 +3,13 @@ import { Link } from "gatsby";
 
 const PostItem = ({ post }) => (
   <li className="post-item">
-    <h1 className="post-item-title">
+    <h2 className="post-item-title">
       <Link
         to={ post.frontmatter.path }
         className="post-item-link"
       >{ post.frontmatter.title }</Link>
-    </h1>
-    <div class="post-item-date"><b>{ post.frontmatter.date }</b></div>
+    </h2>
+    <div className="post-item-date"><b>{ post.frontmatter.date }</b></div>
     <p className="post-item-content">{ post.excerpt }</p>
   </li>
 )
